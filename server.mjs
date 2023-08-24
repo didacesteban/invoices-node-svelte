9a354293308d715cf9ded1143b7bfa22f86a3202
Dídac Esteban Andrés <didac.esteban@artificial-solutions.com>
2023-08-24 14:36:20 +0200

    improvements

--- server.mjs
+++ server.mjs
@@ -5,7 +5,9 @@ import cors from "cors";
 import fs from "fs";
 import puppeteer from "puppeteer";
 import csvtojson from "csvtojson";
+import nodemailer from "nodemailer";
 import { readFile } from "fs/promises";
+import pdfLib from "pdf-lib";
 
 const invoicesJSON = JSON.parse(
   await readFile(new URL("./invoices_db.json", import.meta.url))
@@ -48,6 +50,77 @@ app.get("/invoice/:id", (req, res) => {
   res.json({ ...selectedInvoice });
 });
 
+// app.post("/invoice/download", (req, res) => {
+//   const { invoice } = req.body;
+//   fs.readFile("invoice_template.html", "utf8", function (err, data) {
+//     if (err) {
+//       res.json({ data: "fail" });
+//     }
+
+//     const newInvoice = data
+//       .replaceAll("{date}", invoice.date)
+//       .replace("{id}", invoice.id)
+//       .replace("{name}", invoice.name)
+//       .replace("{dni}", invoice.dni)
+//       .replace("{description}", invoice.description)
+//       .replace("{base}", invoice.base)
+//       .replace("{iva}", invoice.iva)
+//       .replace("{irpf}", invoice.irpf)
+//       .replaceAll("{total}", invoice.total);
+
+//     (async () => {
+//       try {
+//         // Create a new browser instance
+//         const browser = await puppeteer.launch();
+
+//         // Create a new page
+//         const page = await browser.newPage();
+
+//         // Define your local HTML content (replace this with your actual HTML)
+//         const localHtml = newInvoice;
+
+//         // Set the HTML content for the page
+//         await page.setContent(localHtml, { waitUntil: "networkidle0" });
+
+//         // Generate PDF from the page
+//         const pdfBuffer = await page.pdf({ format: "A4" });
+
+//         // Close the browser
+//         await browser.close();
+
+//         // Set up nodemailer
+//         const transporter = nodemailer.createTransport({
+//           service: "Gmail", // Update with your email service
+//           auth: {
+//             user: "didacesteban@gmail.com",
+//             pass: "fswgzsxglxtpnowb",
+//           },
+//         });
+
+//         const mailOptions = {
+//           from: "didacesteban@gmail.com",
+//           to: "valenreyes.nutricion@gmail.com",
+//           subject: "Te puedo mandar una factura desde la aplicacion a tu mail?",
+//           text: "Estoy probando si te puedo mandar una factura a tu email jajaja",
+//           attachments: [
+//             {
+//               filename: `factura_${invoice.id}.pdf`,
+//               content: pdfBuffer,
+//             },
+//           ],
+//         };
+
+//         // Send the email
+//         const info = await transporter.sendMail(mailOptions);
+//         console.log("Email sent:", info.response);
+//       } catch (error) {
+//         console.error("Error:", error);
+//       }
+//       res.json({ data: "email sent" });
+//     })();
+//   });
+// });
+
 app.post("/invoice/download", (req, res) => {
   const { invoice } = req.body;
   fs.readFile("invoice_template.html", "utf8", function (err, data) {
@@ -67,30 +140,34 @@ app.post("/invoice/download", (req, res) => {
       .replaceAll("{total}", invoice.total);
 
     (async () => {
-      // Create a new browser instance
-      const browser = await puppeteer.launch();
+      try {
+        // Create a new browser instance
+        const browser = await puppeteer.launch();
 
-      // Create a new page
-      const page = await browser.newPage();
+        // Create a new page
+        const page = await browser.newPage();
 
-      // Define your local HTML content
-      const localHtml = newInvoice;
+        // Define your local HTML content (replace this with your actual HTML)
+        const localHtml = newInvoice;
 
-      // Set the HTML content for the page
-      await page.setContent(localHtml, { waitUntil: "networkidle0" });
+        // Set the HTML content for the page
+        await page.setContent(localHtml, { waitUntil: "networkidle0" });
 
-      // Generate PDF from the page
-      const pdfBuffer = await page.pdf({ format: "A4" });
+        // Generate PDF from the page
+        const pdfBuffer = await page.pdf({ format: "A4" });
 
-      // Save the PDF to a file using fs
-      fs.writeFileSync(
-        `invoices/factura_${invoice.id}_${invoice.name}.pdf`,
-        pdfBuffer
-      );
+        // Close the browser
+        await browser.close();
 
-      // Close the browser
-      await browser.close();
-      res.json({ data: "HTML file created!" });
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename="{factura_${invoice.id}.pdf"`
+        );
+        res.send(pdfBuffer);
+      } catch (error) {
+        console.error("Error:", error);
+      }
     })();
   });
 });
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,268 +1,176 @@
 <script>
-	import axios from 'axios';
-	import { onMount } from 'svelte';
+  // @ts-nocheck
 
-	let data = [];
-	let selectedInvoice = null;
+  // @ts-ignore
+  import axios from "axios";
+  import { onMount } from "svelte";
 
-	const fetchData = async () => {
-		const response = await fetch('http://localhost:3000/invoice/');
-		const json = await response.json();
-		data = json;
-	};
+  // @ts-ignore
+  /**
+   * @type {any[]}
+   */
+  let data = [];
+  // @ts-ignore
+  /**
+   * @type {{ id: any; date: any; name: any; dni: any; description: any; base: any; iva: any; irpf: any; total: any; } | null}
+   */
 
-	const downloadInvoice = async (invoice) => {
-		axios
-			.post('http://localhost:3000/invoice/download', { invoice })
-			.then((res) => {
-				console.log(res.data);
-			})
-			.catch((err) => {
-				console.log('Error: ', err.message);
-			});
-	};
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:3000/invoice/");
+    const json = await response.json();
+    data = json;
+  };
 
-	const downloadAllInvoices = async (invoices) => {
-		invoices.forEach((invoice) => {
-			if (invoice.id) {
-				axios
-					.post('http://localhost:3000/invoice/download', { invoice })
-					.then((res) => {
-						console.log(res.data);
-					})
-					.catch((err) => {
-						console.log('Error: ', err.message);
-					});
-			}
-		});
-	};
+  // @ts-ignore
+  const downloadInvoice = async function downloadPDF(invoice) {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/invoice/download",
+        { invoice },
+        {
+          responseType: "arraybuffer", // To get binary data as ArrayBuffer
+        }
+      );
 
-	const csvToJson = async (csv) => {
-		axios
-			.post('http://localhost:3000/invoice/csvtojson', { csvData: csv })
-			.then((response) => {
-				data = response.data;
-			})
-			.catch((error) => {
-				console.error('Error:', error.message);
-			});
-	};
+      const pdfBlob = new Blob([response.data], { type: "application/pdf" });
+      const url = URL.createObjectURL(pdfBlob);
 
-	const setSelectedInvoice = (invoice) => (selectedInvoice = invoice);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `factura_${invoice.id}.pdf`;
+      a.click();
 
-	onMount(() => {
-		fetchData();
-	});
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading PDF:", error);
+    }
+  };
+
+  // @ts-ignore
+  const downloadAllInvoices = (invoices) => {
+    // @ts-ignore
+    invoices.forEach((invoice) => {
+      if (invoice.id) {
+        downloadInvoice(invoice);
+      }
+    });
+  };
+
+  // @ts-ignore
+  const csvToJson = async (csv) => {
+    axios
+      .post("http://localhost:3000/invoice/csvtojson", { csvData: csv })
+      // @ts-ignore
+      .then((response) => {
+        data = response.data;
+      })
+      // @ts-ignore
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  };
+
+  onMount(() => {
+    // fetchData();
+  });
 </script>
 
 <svelte:head>
-	<title>Invoices</title>
-	<meta name="description" content="Invoices" />
+  <title>Excel a Factura en pdf</title>
+  <meta name="description" content="Excel a Factura en pdf" />
 </svelte:head>
+<div class="p-4 flex flex-col justify-center min-h-screen max-w-3xl mx-auto">
+  <div class="flex flex-col w-full p-6 bg-sky-100 rounded">
+    <div class="flex items-center justify-center font-black m-3 mb-12">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-10 w-10 mr-3 text-red-600 animate-pulse"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <h1 class="tracking-wide text-3xl text-gray-900">Excel a PDF</h1>
+    </div>
+    {#if !data.length}
+      <label class="text-sm font-medium"
+        ><b>1.</b> Abre tu excel y guarda el documento como CSV. Abre el CSV con
+        un programa para leer texto y copia los datos tal y como aparecen en el archivo,
+        sin cambiar saltos de linea ni nada del formato.</label
+      >
+      <label class="text-sm font-medium mt-4"
+        ><b>2.</b> Abre el CSV con un programa para leer texto y copia los datos
+        tal y como aparecen en el archivo, sin cambiar saltos de linea ni nada del
+        formato.</label
+      >
+      <textarea
+        id="textarea"
+        rows="15"
+        class="mt-4 focus:outline-none w-full
+		mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400"
+        placeholder="Pega aquí el csv de tu tabla de excel"
+      />
+      <button
+        type="button"
+        class="mt-4 button"
+        on:click={() => csvToJson(document.getElementById("textarea").value)}
+        >Cargar datos</button
+      >
+    {/if}
 
-{#if !selectedInvoice}
-	<button
-		type="button"
-		class="button"
-		on:click={() => csvToJson(document.getElementById('textarea').value)}>csv to json</button
-	>
-	<button type="button" class="button" on:click={() => downloadAllInvoices(data)}
-		>Download all invoices</button
-	>
-	<div class="container">
-		<textarea id="textarea" />
-		<table>
-			<!-- <tr>
-			<th>Name</th>
-			<th>Dni</th>
-			<th>Base</th>
-		</tr> -->
-			{#each data as item}
-				{#if item.id && item.id !== ''}
-					<tr>
-						<td>{item.id}</td>
-						<td>{item.name}</td>
-						<td>{item.dni}</td>
-						<td>{item.base}</td>
-						<td><button type="button" on:click={() => setSelectedInvoice(item)}>open</button></td>
-						<td><button type="button" on:click={() => downloadInvoice(item)}>download</button></td>
-					</tr>
-				{/if}
-			{/each}
-		</table>
-	</div>
-{/if}
-
-{#if selectedInvoice}
-	<div on:keydown={() => setSelectedInvoice(null)} on:click={() => setSelectedInvoice(null)}>
-		<table cellpadding="0" cellspacing="0">
-			<tr class="top">
-				<td colspan="6">
-					<table>
-						<tr>
-							<td class="title">
-								<!-- <img src="https://www.sparksuite.com/images/logo.png" style="width: 100%; max-width: 300px" /> -->
-							</td>
-
-							<td style="text-align: right">
-								Factura #: <b>{selectedInvoice.id}</b><br />
-								Fecha: <b>{selectedInvoice.date}</b><br />
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-
-			<tr class="information">
-				<td colspan="6">
-					<table>
-						<tr>
-							<td>
-								María Valentina Reyes Martínez<br />
-								Y3026929-M<br />
-								Calle Gran Canaria 24, Sabadell, 08205<br />
-								692453823<br />
-								valenreyes.nutricion@gmail.com
-							</td>
-
-							<td style="text-align: right">
-								{selectedInvoice.date}<br />
-								{selectedInvoice.name}<br />
-								{selectedInvoice.dni}
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-
-			<tr class="heading">
-				<td>Cantidad</td>
-				<td>Descripción</td>
-				<td>Precio unitario</td>
-				<td>IVA (21%)</td>
-				<td>IRPF (15%)</td>
-				<td style="text-align: right">Importe</td>
-			</tr>
-
-			<tr class="item">
-				<td>1</td>
-				<td>{selectedInvoice.description}</td>
-				<td>{selectedInvoice.base}</td>
-				<td>{selectedInvoice.iva}</td>
-				<td>{selectedInvoice.irpf}</td>
-				<td style="text-align: right">{selectedInvoice.total}</td>
-			</tr>
-
-			<tr class="total">
-				<td />
-				<td />
-				<td />
-				<td />
-				<td />
-				<td style="text-align: right">Total: <b>{selectedInvoice.total}</b></td>
-			</tr>
-		</table>
-	</div>
-{/if}
-
-<style>
-	table {
-		font-family: arial, sans-serif;
-		border-collapse: collapse;
-		width: 100%;
-	}
-
-	td,
-	th {
-		border: 1px solid #dddddd;
-		text-align: left;
-		padding: 8px;
-	}
-
-	tr:nth-child(even) {
-		background-color: #dddddd;
-	}
-	.invoice-box {
-		max-width: 800px;
-		margin: auto;
-		padding: 30px;
-		border: 1px solid #eee;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-		font-size: 16px;
-		line-height: 24px;
-		font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-		color: #555;
-	}
-
-	.invoice-box table {
-		width: 100%;
-		line-height: inherit;
-		/* text-align: left; */
-	}
-
-	.invoice-box table td {
-		padding: 5px;
-		vertical-align: top;
-	}
-
-	.invoice-box table tr td:nth-child(2) {
-		/* text-align: right; */
-	}
-
-	.invoice-box table tr.top table td {
-		padding-bottom: 20px;
-	}
-
-	.invoice-box table tr.top table td.title {
-		font-size: 45px;
-		line-height: 45px;
-		color: #333;
-	}
-
-	.invoice-box table tr.information table td {
-		padding-bottom: 40px;
-	}
-
-	.invoice-box table tr.heading td {
-		background: #eee;
-		border-bottom: 1px solid #ddd;
-		font-weight: bold;
-	}
-
-	.invoice-box table tr.details td {
-		padding-bottom: 20px;
-	}
-
-	.invoice-box table tr.item td {
-		border-bottom: 1px solid #eee;
-	}
-
-	.invoice-box table tr.item.last td {
-		border-bottom: none;
-	}
-
-	.invoice-box table tr.total td:nth-child(2) {
-		/* border-top: 2px solid #eee; */
-		font-weight: bold;
-	}
-
-	@media only screen and (max-width: 600px) {
-		.invoice-box table tr.top table td {
-			width: 100%;
-			display: block;
-			/* text-align: center; */
-		}
-
-		.invoice-box table tr.information table td {
-			width: 100%;
-			display: block;
-			/* text-align: center; */
-		}
-	}
-
-	/** RTL **/
-	.invoice-box.rtl {
-		direction: rtl;
-		font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-	}
-</style>
+    {#if data.length}
+      <label class="text-sm font-medium mt-4"
+        >Para descargar todas las facturas en pdf pulsa el siguiente boton:</label
+      >
+      <button
+        type="button"
+        class="px-4 button mt-4"
+        on:click={() => downloadAllInvoices(data)}
+        >Descargar todas las facturas</button
+      >
+      <table class="mt-4">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Dni</th>
+          <th>Base</th>
+          <th class="text-center">Descargar</th>
+        </tr>
+        {#each data as item}
+          {#if item.id && item.id !== ""}
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.dni}</td>
+              <td>{item.base}</td>
+              <td class="text-center"
+                ><button
+                  type="button"
+                  class="button"
+                  on:click={() => downloadInvoice(item)}
+                  ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-arrow-down-square"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
+                    />
+                  </svg></button
+                ></td
+              >
+            </tr>
+          {/if}
+        {/each}
+      </table>
+    {/if}
+  </div>
+</div>
