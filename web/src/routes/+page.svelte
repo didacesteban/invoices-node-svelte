d0ce848720ab9fa981d62a6af8cead5c273e8fe7
Dídac Esteban Andrés <didac.esteban@artificial-solutions.com>
2023-09-27 16:12:47 +0200

    add loading message. Fix chrome issue

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,8 +3,8 @@
   // @ts-ignore
   import axios from "axios";
 
-  const api = "https://invoices-node-svelte-production.up.railway.app";
-  // const api = "http://localhost:3000";
+  // const api = "https://invoices-node-svelte-production.up.railway.app";
+  const api = "http://localhost:3000";
 
   // @ts-ignore
   /**
@@ -13,6 +13,7 @@
   let data = [];
   let admin = { name: "", id: "", adress: "", phone: "", email: "" };
   let selectedFile;
+  let loading = false;
   // @ts-ignore
   /**
    * @type {{ id: any; date: any; name: any; dni: any; description: any; base: any; iva: any; irpf: any; total: any; } | null}
@@ -35,7 +36,14 @@
       console.error("Error downloading PDF:", error);
     }
   };
-  const downloadInvoice = async function downloadPDF(invoice) {
+
+  function pause(msec) {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, msec || 1000);
+    });
+  }
+
+  const downloadInvoice = async function downloadPDF(invoice, isLastInvoice) {
     try {
       const response = await axios.post(`${api}/invoice/download`, {
         invoice,
@@ -45,34 +53,40 @@
       const options = {
         filename: `factura_${invoice.id}.pdf`,
         margin: 10,
-        html2canvas: { width: 1000, useCORS: true },
+        // html2canvas: { width: 1000, useCORS: true },
       };
 
-      const pdf = await html2pdf()
-        .from(response.data)
-        .set(options)
-        .toPdf()
-        .get("pdf");
-      let link = document.createElement("a");
-      link.target = "_blank";
-      link.href = pdf.output("bloburl");
-      link.download = `factura_${invoice.id}.pdf`;
-      link.click();
-      link.remove();
+      if (invoice.id) {
+        html2pdf().from(response.data).set(options).toPdf().save();
+      }
+      // let link = document.createElement("a");
+      // link.target = "_blank";
+      // link.href = pdf.output("bloburl");
+      // link.download = `factura_${invoice.id}.pdf`;
+      // link.click();
+      // link.remove();
+      if (isLastInvoice) {
+        loading = false;
+      }
     } catch (error) {
       console.error("Error downloading PDF:", error);
     }
   };
 
   // @ts-ignore
-  const downloadAllInvoices = (invoices) => {
-    // @ts-ignore
-    invoices.forEach((invoice) => {
-      if (invoice.id) {
-        downloadInvoice(invoice);
+  async function downloadAllInvoices(invoices) {
+    let count = 0;
+    loading = true;
+    for (let i = 0; i < invoices.length; i++) {
+      const invoice = invoices[i];
+      downloadInvoice(invoice, i === invoices.length - 1);
+
+      if (++count >= 5) {
+        await pause(1500);
+        count = 0;
       }
-    });
-  };
+    }
+  }
 
   async function uploadFile() {
     const formData = new FormData();
@@ -218,12 +232,38 @@
       <label class="text-sm font-medium mt-4"
         >Para descargar todas las facturas en pdf pulsa el siguiente boton:</label
       >
-      <button
-        type="button"
-        class="px-4 button mt-4"
-        on:click={() => downloadAllInvoices(data)}
-        >Descargar todas las facturas</button
-      >
+      {#if !loading}
+        <button
+          type="button"
+          class="px-4 button mt-4"
+          on:click={() => downloadAllInvoices(data)}
+          >Descargar todas las facturas</button
+        >
+      {/if}
+      {#if loading}
+        <div class="inline-flex items-center justify-center px-4 button mt-4">
+          <svg
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            />
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            />
+          </svg> Descargando facturas...
+        </div>
+      {/if}
       <table class="mt-4">
         <tr>
           <th>Nº</th>
